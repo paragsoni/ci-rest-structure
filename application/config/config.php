e2b32eb7181cfc8b489f7a5fe45d82b0ba1209c8
paragsoniagile <37929934+paragsoniagile@users.noreply.github.com>
2018-10-02 11:16:35 +0530

    Update config.php
    1. Auto set base url, so no need to every time server change.
    2. Remove index.php
    
    Notes:
    If you have an Amazon Load Balancer (EBL) the variables $_SERVER['HTTPS']) and $_SERVER['HTTPS'] are unavailable, the solution is simple:
    
    $config['base_url']  = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") || (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on')) ?  "https" : "http";
    $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
    $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,10 @@ date_default_timezone_set('UTC');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+
+$root = "http://".$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url']    = "$root";
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +38,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
